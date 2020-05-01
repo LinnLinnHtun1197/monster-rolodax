366e9ee37a2120824311b99be59ee7e4a9a2a70b
LinnLinnHtun197 <htun.linn@infinitycore.sg>
2020-05-01 13:03:48 +0800

    Updates

---
+++ precache-manifest.f518ccd5d26fa5fe7584cf890e4986bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be212f2fc4b6367615974f6a23b51b93",
+    "url": "/monster-rolodax/index.html"
+  },
+  {
+    "revision": "0953e0886b088ce8fa39",
+    "url": "/monster-rolodax/static/css/main.a203bdab.chunk.css"
+  },
+  {
+    "revision": "295f9cce0c4959a17201",
+    "url": "/monster-rolodax/static/js/2.041c4c82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodax/static/js/2.041c4c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0953e0886b088ce8fa39",
+    "url": "/monster-rolodax/static/js/main.fb8fe763.chunk.js"
+  },
+  {
+    "revision": "ac545d41376cfa43d53c",
+    "url": "/monster-rolodax/static/js/runtime-main.0862a724.js"
+  }
+]);
